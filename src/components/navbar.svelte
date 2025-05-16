4861bcfc9415876c31892a3f71bd4b53bb452efe
Zenrieru <nvll.zen@gmail.com>
2025-05-16 15:30:40 +0700

    HTMLed Navbar and Page 1

---
+++ src/components/navbar.svelte
@@ -0,0 +1,35 @@
+<div class="navbar">
+    <h1 class="navbarTitle">WasteWay</h1>
+    <ul class="navbarList">
+        <li class="navbarLinks">About</li>
+        <li class="navbarLinks">News</li>
+        <li class="navbarLinks">Join Us</li>
+    </ul>
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        flex-direction: row;
+        width: 100vw;
+        height: fit-content;
+        align-items: center;
+    }
+
+    .navbarTitle {
+        width: 15%;
+    }
+
+    .navbarList {
+        width: 80%;
+        display: flex;
+        flex-direction: row;
+        list-style-type: none;
+        justify-content: right;
+        gap: 20px;
+    }
+
+    .navbarLinks {
+        font-size: 1.5rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "../styles/global.css"
+    import Navbar from "../components/navbar.svelte";
+</script>
+
+<Navbar />
+<div class="page-section">
+    <h1 id="page-1-header">Cleaning the city from waste</h1>
+    <h1>one way at a moment</h1>
+</div>
